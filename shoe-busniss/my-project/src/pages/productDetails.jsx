61cbd5c5fadd945e82e7066acfecc5c50f9d581f
sivan <sivanhannan7@gmail.com>
2025-02-14 11:23:11 +0100

    detailPage update css

--- shoe-busniss/my-project/src/pages/productDetails.jsx
+++ shoe-busniss/my-project/src/pages/productDetails.jsx
@@ -19,8 +19,8 @@ function ProductDetails(){
                     <p className= {styles.productName}>{product.name}</p>
                     <div className= {styles.info}>
                         <p className= {styles.originalPrice}>Original price: <span>&euro;{product.originalprice}</span></p>
-                        <p className= {styles.discountPrice}>New price: &euro;{product.discountprice}</p>
-                        <p className= {styles.colors}>colors: 1</p>
+                        <p className= {styles.discountPrice}>New price: <span>&euro;{product.discountprice}</span></p>
+                        <p className= {styles.colors}>colors: <span>1</span></p>
                         <p className= {styles.availability}>Availability: <span>In Stock</span></p>
                     </div>
                     <p className= {styles.discountPercent}>{product.discountprocent}% off</p>
