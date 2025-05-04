b0f174e216ed2485434591b648e4addb9ecc5c3f
sivan <sivanhannan7@gmail.com>
2025-05-04 16:13:08 +0200

    vite cinfig update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// ✅ base must be exact repository name with slashes
+// base must be exact repository name with slashes
 export default defineConfig({
   base: '/Dandido/',
   plugins: [react()],
